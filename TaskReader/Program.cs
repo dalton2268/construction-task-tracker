5d101e380dfa73e3e3e93b250669d94e8ef658cb
Dalton <dalton@gmail.com>
2025-07-04 13:12:40 -0400

    Added C# console app to read and display tasks from XML

---
+++ TaskReader/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+var xml = XDocument.Load("tasks.xml");
+Console.WriteLine();
+
+foreach (var task in xml.Descendants("task"))
+{
+    string name = task.Element("name")?.Value ?? "N/A";
+    string assignedTo = task.Element("assignedTo")?.Value ?? "N/A";
+    string status = task.Element("status")?.Value ?? "N/A";
+    string dueDate = task.Element("dueDate")?.Value ?? "N/A";
+
+    Console.WriteLine($"Task: {name}");
+    Console.WriteLine($"Assigned To: {assignedTo}");
+    Console.WriteLine($"Status: {status}");
+    Console.WriteLine($"Due Date: {dueDate}");
+    Console.WriteLine();
+}
